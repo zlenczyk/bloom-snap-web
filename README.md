7ce150bccda2b579d5a23f98a30d3a39be1d073e
Zuzanna Lenczyk - Wąsowska <zuzanna.lenczyk-wasowska@sandstream.pl>
2024-05-30 20:41:38 +0200

    basic next.js, typescript and scss project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# bloom-snap-web
-Diploma thesis app for creating a wall and sharing your collection of houseplants with others
---
+++ bloom-snap/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello from Bloom Snap!</h1>
+    </main>
+  );
+}
