01b366a408088fdc3d0ecd0c2237d9718ce10180
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-06-16 18:30:08 +0200

    configure project for shadcn/ui and remove scss

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
