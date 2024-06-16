f29a53da9408be6b8a8d71c389ec7d8bd68d96fe
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-06-16 18:30:49 +0200

    create simple header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full top-0">
+      <div className="container">Login</div>
+    </header>
+  );
+};
+
+export default Header;
