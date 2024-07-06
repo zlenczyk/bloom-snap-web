ae6a3bcc145a6257fb2a024821a98551a547dd8a
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-07-06 20:13:49 +0200

    add Viewport object to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./_styles";
 import Header from "../components/Header/Header";
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "BloomSnap",
   description: "Capture and share your houseplant haven",
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  viewportFit: "cover",
+  width: "device-width",
 };
 
 const RootLayout = ({
