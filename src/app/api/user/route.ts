0f10b02b85191b529fb46cdb7c8534f284df4365
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-07-13 20:12:17 +0200

    hash user password to make it secure

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,5 @@
 import db from "@/lib/db";
+import { hash } from "bcrypt";
 
 export async function POST(req: Request) {
   try {
@@ -29,11 +30,13 @@ export async function POST(req: Request) {
       );
     }
 
+    const hashedPassword = await hash(password, 10);
+
     const newUser = await db.user.create({
       data: {
         username,
         email,
-        password,
+        password: hashedPassword,
       },
     });
 
