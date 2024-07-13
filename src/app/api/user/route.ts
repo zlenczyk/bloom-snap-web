e3e04ac0878782b106fb5ac57ce51a1833a8b7ff
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-07-13 20:04:46 +0200

    create user api route for creating new user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,55 @@
+import db from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email, username, password } = body;
+
+    const existingEmail = await db.user.findUnique({ where: { email: email } });
+    if (existingEmail) {
+      return Response.json(
+        {
+          user: null,
+          message: "User with this email already exist",
+        },
+        { status: 409 }
+      );
+    }
+
+    const existingUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+    if (existingUsername) {
+      return Response.json(
+        {
+          user: null,
+          message: "User with this username already exist",
+        },
+        { status: 409 }
+      );
+    }
+
+    const newUser = await db.user.create({
+      data: {
+        username,
+        email,
+        password,
+      },
+    });
+
+    return Response.json(
+      { user: newUser, message: "User created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Failed to create user: ", error);
+
+    return Response.json(
+      {
+        user: null,
+        message: "An error occurred while creating the user",
+      },
+      { status: 500 }
+    );
+  }
+}
