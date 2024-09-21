4d2f306fa1a1aa928be5385adf70f139351b70cc
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2024-09-21 23:18:45 +0200

    add authorized callback to NextAuth callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -23,6 +23,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/sign-in",
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+
+      console.log("is logged in: ", isLoggedIn);
+
+      if (isOnDashboard) {
+        if (isLoggedIn) {
+          return true;
+        }
+
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
   providers: [
     Credentials({
       credentials: {
