223a83437aff497b7fbfc2556ad4df9589bb47c7
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-05-04 14:07:10 +0200

    preprocess all "empty" values to null in zod schema for adding plant

--- src/app/add-plant/schema.ts
+++ src/app/add-plant/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, z } from "zod";
+import { z } from "zod";
 
 const fileSchema = z.object({
   name: z.string(),
@@ -6,318 +6,159 @@ const fileSchema = z.object({
   type: z.enum(["image/jpeg", "image/png", "application/pdf"]),
 });
 
-// const AddPlantFormSchema = z
-//   .object({
-//     species: z
-//       .string()
-//       .min(2, { message: "Species must have at least 2 characters." })
-//       .max(100, { message: "Species too long. Use up to 100 characters." })
-//       .optional(),
-
-//     genus: z
-//       .string()
-//       .min(2, { message: "Genus must have at least 2 characters." })
-//       .max(100, { message: "Genus too long. Use up to 100 characters." })
-//       .optional(),
-
-//     commonName: z
-//       .string()
-//       .min(2, { message: "Common name must have at least 2 characters." })
-//       .max(100, { message: "Common name too long. Use up to 100 characters." })
-//       .optional(),
-
-//     nickname: z
-//       .string()
-//       .min(2, { message: "Nickname must have at least 2 characters." })
-//       .max(100, { message: "Nickname too long. Use up to 100 characters." })
-//       .optional(),
-
-//     description: z
-//       .string()
-//       .min(2, {
-//         message: "Custom description must have at least 2 characters.",
-//       })
-//       .max(250, {
-//         message: "Custom description too long. Use up to 250 characters.",
-//       })
-//       .optional(),
-
-//     source: z
-//       .string()
-//       .min(2, { message: "Source must have at least 2 characters." })
-//       .max(100, { message: "Source too long. Use up to 100 characters." })
-//       .optional(),
-
-//     date: z
-//       .date()
-//       .min(new Date("1900-01-01"), { message: "Date must be after 1900" })
-//       .max(new Date(), { message: "Date cannot be in the future" })
-//       .optional(),
-
-//     lastRepotted: z
-//       .date()
-//       .min(new Date("1900-01-01"), { message: "Date must be after 1900" })
-//       .max(new Date(), { message: "Date cannot be in the future" })
-//       .optional(),
-
-//     roomLocation: z
-//       .string()
-//       .min(2, { message: "Room name must have at least 2 characters." })
-//       .max(50, { message: "Room name too long. Use up to 50 characters." })
-//       .optional(),
-
-//     isPetSafe: z
-//       .enum(["yes", "no"], {
-//         message: "Select whether the plant is safe for pets.",
-//       })
-//       .optional(),
-
-//     isHealthy: z
-//       .string()
-//       .min(2, { message: "Health status must have at least 2 characters." })
-//       .max(300, {
-//         message: "Health status too long. Use up to 300 characters.",
-//       })
-//       .optional(),
-
-//     windowDirection: z
-//       .enum(
-//         [
-//           "north",
-//           "south",
-//           "east",
-//           "west",
-//           "northeast",
-//           "northwest",
-//           "southeast",
-//           "southwest",
-//           "artificial",
-//         ],
-//         { message: "Select a valid window direction." }
-//       )
-//       .optional(),
-
-//     isBlooming: z
-//       .enum(["yes", "no"], {
-//         message: "Select whether the plant is blooming.",
-//       })
-//       .optional(),
-
-//     isAirCleaning: z
-//       .enum(["yes", "no"], {
-//         message: "Select whether the plant is an air cleaner.",
-//       })
-//       .optional(),
-
-//     pictures: z
-//       .array(fileSchema)
-//       .max(5, "You can upload up to 5 pictures")
-//       .optional(),
-//   })
-//   .refine(
-//     (data) => {
-//       return data.species || data.genus || data.commonName || data.nickname;
-//     },
-//     {
-//       message:
-//         "At least one of species, genus, common name, or nickname is required",
-//       path: ["species", "genus", "commonName", "nickname"], // Show error for these fields
-//     }
-//   );
-
-// const AddPlantFormSchema = z
-//   .object({
-//     species: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(100).nullable()
-//     ),
-//     genus: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(100).nullable()
-//     ),
-//     commonName: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(100).nullable()
-//     ),
-//     nickname: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(100).nullable()
-//     ),
-//     description: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(250).nullable()
-//     ),
-//     source: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(100).nullable()
-//     ),
-//     ownedSince: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.date().min(new Date("1900-01-01")).max(new Date()).nullable()
-//     ),
-//     lastRepotted: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.date().min(new Date("1900-01-01")).max(new Date()).nullable()
-//     ),
-//     roomLocation: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(50).nullable()
-//     ),
-
-//     isPetSafe: z.preprocess(
-//       (val) =>
-//         val === "true"
-//           ? true
-//           : val === "false"
-//           ? false
-//           : val === ""
-//           ? null
-//           : val,
-//       z.enum(["true", "false", ""]).nullable()
-//     ),
-
-//     isHealthy: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z.string().min(2).max(300).nullable()
-//     ),
-//     windowDirection: z.preprocess(
-//       (val) => (val === "" ? null : val),
-//       z
-//         .enum([
-//           "",
-//           "north",
-//           "south",
-//           "east",
-//           "west",
-//           "northeast",
-//           "northwest",
-//           "southeast",
-//           "southwest",
-//           "artificial",
-//         ])
-//         .nullable()
-//     ),
-//     isBlooming: z.preprocess(
-//       (val) =>
-//         val === "true"
-//           ? true
-//           : val === "false"
-//           ? false
-//           : val === ""
-//           ? null
-//           : val,
-//       z.enum(["true", "false", ""]).nullable()
-//     ),
-
-//     isAirCleaning: z.preprocess(
-//       (val) =>
-//         val === "true"
-//           ? true
-//           : val === "false"
-//           ? false
-//           : val === ""
-//           ? null
-//           : val,
-//       z.enum(["true", "false", ""]).nullable()
-//     ),
-//   })
-//   .refine(
-//     (data) => {
-//       return (
-//         data.species ||
-//         data.genus ||
-//         data.commonName ||
-//         data.nickname ||
-//         data.description ||
-//         data.source
-//       );
-//     },
-//     {
-//       message:
-//         "At least one of species, genus, common name, or nickname is required",
-//       path: ["species", "genus", "commonName", "nickname"], // Show error for these fields
-//     }
-//   );
-
 const AddPlantFormSchema = z.object({
   commonName: z
     .string()
+    .min(1, { message: "Common name is required." })
     .max(100, { message: "Common name too long. Use up to 100 characters." }),
 
   species: z
-    .string()
-    .max(100, { message: "Species too long. Use up to 100 characters." })
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(100, { message: "Species too long. Use up to 100 characters." })
+        .nullable()
+    )
     .optional(),
 
   genus: z
-    .string()
-    .max(100, { message: "Genus too long. Use up to 100 characters." })
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(100, { message: "Genus too long. Use up to 100 characters." })
+        .nullable()
+    )
     .optional(),
 
   nickname: z
-    .string()
-    .max(100, { message: "Nickname too long. Use up to 100 characters." })
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(100, { message: "Nickname too long. Use up to 100 characters." })
+        .nullable()
+    )
     .optional(),
 
   description: z
-    .string()
-    .max(250, {
-      message: "Custom description too long. Use up to 250 characters.",
-    })
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(250, {
+          message: "Custom description too long. Use up to 250 characters.",
+        })
+        .nullable()
+    )
     .optional(),
 
   source: z
-    .string()
-    .max(100, { message: "Source too long. Use up to 100 characters." })
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(100, { message: "Source too long. Use up to 100 characters." })
+        .nullable()
+    )
     .optional(),
 
   ownedSince: z.coerce
     .date()
-    .min(new Date("1900-01-01"), { message: "Date must be after 1900" })
-    .max(new Date(), { message: "Date cannot be in the future" })
+    .min(new Date("1900-01-01"), { message: "Date must be after 1900." })
+    .max(new Date(), { message: "Date cannot be in the future." })
     .optional(),
 
   lastRepotted: z.coerce
     .date()
-    .min(new Date("1900-01-01"), { message: "Date must be after 1900" })
-    .max(new Date(), { message: "Date cannot be in the future" })
+    .min(new Date("1900-01-01"), { message: "Date must be after 1900." })
+    .max(new Date(), { message: "Date cannot be in the future." })
     .optional(),
 
   roomLocation: z
-    .string()
-    .max(50, { message: "Room name too long. Use up to 50 characters." })
-    .optional(),
-
-  isPetSafe: z.enum(["true", "false", "null"]),
-
-  isHealthy: z.preprocess((val) => {
-    if (val === "true") return true;
-    if (val === "false") return false;
-    if (val === "null") return null;
-    return val;
-  }, z.boolean().nullable()),
-
-  windowDirection: z
-    .enum(
-      [
-        "null",
-        "north",
-        "south",
-        "east",
-        "west",
-        "northeast",
-        "northwest",
-        "southeast",
-        "southwest",
-        "artificial",
-      ],
-      { message: "Select a valid window direction." }
+    .preprocess(
+      (value) => (value === "" ? null : value),
+      z
+        .string()
+        .max(100, { message: "Room name too long. Use up to 100 characters." })
+        .nullable()
     )
     .optional(),
 
-  isBlooming: z.enum(["true", "false", "null"]),
+  isPetSafe: z
+    .preprocess((val) => {
+      if (val === "true") return true;
+      if (val === "false") return false;
+      if (val === "null" || val === "") return null;
+      return val;
+    }, z.boolean().nullable())
+    .optional(),
 
-  isAirCleaning: z.enum(["true", "false", "null"]),
+  isHealthy: z
+    .preprocess((val) => {
+      if (val === "true") return true;
+      if (val === "false") return false;
+      if (val === "null" || val === "") return null;
+      return val;
+    }, z.boolean().nullable())
+    .optional(),
+
+  windowDirection: z
+    .preprocess(
+      (val) => {
+        if (val === "null" || val === "") {
+          return null;
+        }
+        return val;
+      },
+      z.enum([
+        "north-facing-northern-hemisphere", // North-facing (no direct sun in Northern Hemisphere)
+        "south-facing-northern-hemisphere", // South-facing (full day of sun in Northern Hemisphere)
+        "east-facing-northern-hemisphere", // East-facing (morning sun, partial shade, good for plants)
+        "west-facing-northern-hemisphere", // West-facing (afternoon sun, tends to be harsher)
+
+        "north-facing-southern-hemisphere", // North-facing (full day of sun in Southern Hemisphere)
+        "south-facing-southern-hemisphere", // South-facing (no direct sun in Southern Hemisphere)
+        "east-facing-southern-hemisphere", // East-facing (morning sun, partial shade, good for plants)
+        "west-facing-southern-hemisphere", // West-facing (afternoon sun, tends to be harsher)
+      ])
+    )
+    .optional(),
+
+  //add window exposure
+
+  //   lightExposure: z
+  // .preprocess((val) => {
+  //   if (val === "null" || val === "") return null;
+  //   return val;
+  // }, z.enum([
+  //   "morning-sun-light",     // gentle light in the morning (usually east-facing)
+  //   "afternoon-sun-light",   // stronger light in the afternoon (usually west-facing)
+  //   "full-day-sun-light",    // full direct sunlight throughout the day (usually south-facing in the Northern Hemisphere)
+  //   "low-sun-light",         // low amount of natural sunlight (often north-facing)
+  //   "indirect-light",        // diffused or reflected light, not directly hitting the plant
+  //   "artificial-light",      // no natural light, only from bulbs or grow lights
+  // ]))
+  // .optional(),
+
+  isBlooming: z
+    .preprocess((val) => {
+      if (val === "true") return true;
+      if (val === "false") return false;
+      if (val === "null" || val === "") return null;
+      return val;
+    }, z.boolean().nullable())
+    .optional(),
+
+  isAirCleaning: z
+    .preprocess((val) => {
+      if (val === "true") return true;
+      if (val === "false") return false;
+      if (val === "null" || val === "") return null;
+      return val;
+    }, z.boolean().nullable())
+    .optional(),
 
   // pictures: z
   //   .array(fileSchema)
