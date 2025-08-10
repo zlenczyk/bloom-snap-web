925565d05fa6a2fe1b182106ce122e6e533eb172
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-08-10 21:46:25 +0200

    create tags component (choosed options) for multiselect tag input

---
+++ src/components/MultiSelectTagInput/Tags.tsx
@@ -0,0 +1,38 @@
+import { X as ClearIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+type TagsProps = {
+  selectedItems: string[];
+  toggleOption: (item: string) => void;
+};
+
+const Tags = ({ selectedItems, toggleOption }: TagsProps) => {
+  const empty = selectedItems.length === 0;
+
+  return (
+    <div className="flex flex-wrap gap-1">
+      {empty ? (
+        <span className="text-muted-foreground">Select options...</span>
+      ) : (
+        selectedItems.map((item, index) => (
+          <Badge
+            key={`${item}-${index}`}
+            variant="secondary"
+            className="mr-1 mb-1 [&>svg]:pointer-events-auto"
+          >
+            {item}
+            <ClearIcon
+              className="h-3 w-3 text-muted-foreground hover:text-foreground cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleOption(item);
+              }}
+            />
+          </Badge>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Tags;
