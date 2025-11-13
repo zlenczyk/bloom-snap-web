73d6af9fad6eff85e248b6b5555e3c03d0ff2362
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-11-14 00:10:12 +0100

    create plant details page

---
+++ src/app/my-collection/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { getTimelineEvents } from "@/lib/db/queries/getTimelineEvents";
+import PlantDetails from "./PlantDetails/PlantDetails";
+import Timeline from "./Timeline/Timeline";
+import { getPlantDetails } from "./PlantDetails/actions";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const events = await getTimelineEvents();
+
+  const { id: plantId } = await params;
+  const plant = await getPlantDetails(plantId);
+
+  return (
+    <div className="w-full">
+      <PlantDetails plant={plant} />
+      <Timeline initialEvents={events} />
+    </div>
+  );
+};
+
+export default Page;
