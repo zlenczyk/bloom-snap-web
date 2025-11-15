f53691e27a9a9bc3685299c84c5cc39aca89f96d
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-11-15 20:12:49 +0100

    fix Environment component to not let text stretch outside the wrapper

--- src/app/my-collection/[id]/PlantDetails/Environment.tsx
+++ src/app/my-collection/[id]/PlantDetails/Environment.tsx
@@ -1,4 +1,12 @@
 import {
+  GROWING_MEDIUM_OPTIONS,
+  GrowingMediumEnum,
+  LIGHT_EXPOSURE_OPTIONS,
+  LightExposureEnum,
+  WINDOW_DIRECTION_OPTIONS,
+  WindowDirectionEnum,
+} from "@/lib/data/plantDetailsTypes";
+import {
   Cloud,
   Compass,
   MapPin,
@@ -13,10 +21,10 @@ interface EnvironmentProps {
   environment: {
     humidity: string | null;
     temperature: string | null;
-    lightExposure: string | null;
-    windowDirection: string | null;
+    lightExposure: `${LightExposureEnum}` | null;
+    windowDirection: `${WindowDirectionEnum}` | null;
     roomLocation: string | null;
-    growingMedium: string | null;
+    growingMedium: `${GrowingMediumEnum}` | null;
     pottingMix: string[];
   };
 }
@@ -35,46 +43,78 @@ const Environment = ({ environment }: EnvironmentProps) => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-3 mb-3">
-        <div className="flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-cyan-400/40 to-cyan-50 text-center shadow-md">
+        <div className="flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-cyan-400/40 to-cyan-50 text-center shadow-md min-w-0 w-full">
           <Cloud className="h-5 w-5 text-cyan-800 mb-1" />
           <p className="text-sm text-gray-600">Humidity</p>
-          <ValueDisplay value={environment.humidity} variant="stats" />
+          <div className="w-full">
+            <ValueDisplay value={environment.humidity} variant="stats" />
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-rose-400/40 to-rose-50 text-center shadow-md">
+        <div className="flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-rose-400/40 to-rose-50 text-center shadow-md min-w-0 w-full">
           <Thermometer className="h-5 w-5 text-rose-800 mb-1" />
-          <p className="text-sm  text-gray-600">Temperature</p>
-          <ValueDisplay value={environment.temperature} variant="stats" />
+          <p className="text-sm text-gray-600">Temperature</p>
+          <div className="w-full">
+            <ValueDisplay value={environment.temperature} variant="stats" />
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center p-3 rounded-lg text-center shadow-md bg-gradient-to-br from-amber-300/60 to-amber-50">
+        <div className="flex flex-col items-center justify-center p-3 rounded-lg text-center shadow-md bg-gradient-to-br from-amber-300/60 to-amber-50 min-w-0 w-full">
           <Sun className="h-5 w-5 text-amber-800 mb-1" />
-          <p className="text-sm  text-gray-600">Light</p>
-          <ValueDisplay value={environment.lightExposure} variant="stats" />
+          <p className="text-sm text-gray-600">Light</p>
+          <div className="w-full">
+            <ValueDisplay
+              value={
+                environment.lightExposure
+                  ? LIGHT_EXPOSURE_OPTIONS[environment.lightExposure]?.label
+                  : null
+              }
+              variant="stats"
+            />
+          </div>
         </div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
         <div className="flex flex-col gap-3 h-full">
-          <div className="flex-1 flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-blue-400/40 to-blue-50 text-center shadow-md">
+          <div className="flex-1 flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-blue-400/40 to-blue-50 text-center shadow-md min-w-0 w-full">
             <Compass className="h-5 w-5 text-blue-800 mb-1" />
-            <p className="text-sm  text-gray-600">Window</p>
-            <ValueDisplay value={environment.windowDirection} variant="stats" />
+            <p className="text-sm text-gray-600">Window</p>
+            <div className="w-full">
+              <ValueDisplay
+                value={
+                  environment.windowDirection
+                    ? WINDOW_DIRECTION_OPTIONS[environment.windowDirection]
+                        ?.label
+                    : null
+                }
+                variant="stats"
+              />
+            </div>
           </div>
 
-          <div className="flex-1 flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-orange-600/20 to-orange-50 text-center shadow-md">
+          <div className="flex-1 flex flex-col items-center justify-center p-3 rounded-lg bg-gradient-to-br from-orange-600/20 to-orange-50 text-center shadow-md min-w-0 w-full">
             <MapPin className="h-5 w-5 text-orange-800 mb-1" />
             <p className="text-sm text-gray-600">Location</p>
-            <ValueDisplay value={environment.roomLocation} variant="stats" />
+            <div className="w-full">
+              <ValueDisplay value={environment.roomLocation} variant="stats" />
+            </div>
           </div>
         </div>
 
-        <GrowingMedium
-          mediumType={environment.growingMedium}
-          items={environment.pottingMix}
-        />
+        <div className="min-w-0 w-full">
+          <GrowingMedium
+            mediumType={
+              environment.growingMedium
+                ? GROWING_MEDIUM_OPTIONS[environment.growingMedium]?.label
+                : null
+            }
+            items={environment.pottingMix}
+          />
+        </div>
       </div>
     </section>
   );
 };
+
 export default Environment;
