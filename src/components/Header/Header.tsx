10e54e549a410a568a444f37e191df648e39b806
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-12-29 00:55:24 +0100

    show menu only if user is log in and remove sign in button, adjust styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,37 @@
 import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import { LogIn } from "lucide-react";
+import db from "@/lib/db";
 import Link from "next/link";
 import Menu from "./Menu";
 
 const Header = async () => {
   const session = await auth();
 
-  const user = session?.user;
+  let user;
+
+  if (session?.user?.id) {
+    user = await db.user.findUnique({
+      where: { id: session.user.id },
+      select: {
+        image: true,
+        userName: true,
+        profileCompleted: true,
+      },
+    });
+  }
 
   return (
-    <header className="w-full h-header-height fixed top-0 z-50">
-      <div className="absolute inset-0 bg-white"></div>
-      <div className="absolute inset-0 bg-gradient-to-br from-green-500/40 to-green-100 shadow-md"></div>
-      <div className="relative flex items-center justify-between mx-auto w-full max-w-7xl px-6 py-2 h-full">
+    <header className="w-full h-header-height fixed top-0 z-50 px-6">
+      <div className="absolute inset-0 bg-gradient-to-br from-green-200 to-green-100 shadow-md"></div>
+      <div className="relative flex items-center justify-between mx-auto w-full max-w-7xl py-2 h-full">
         <Link
           href="/"
-          className="flex items-center justify-center text-xl font-extrabold italic text-green-700"
+          className="flex items-center justify-center text-xl sm:text-2xl font-extrabold italic text-green-700"
         >
           BloomSnap
         </Link>
 
-        {user ? (
-          <Menu
-            userImage="/assets/placeholder-profile.png"
-            userName="Jane Doe"
-          />
-        ) : (
-          <Button asChild className="bg-zinc-800 hover:bg-zinc-700">
-            <Link href="/sign-in">
-              <LogIn className="mr-2 h-4 w-4" />
-              Sign in
-            </Link>
-          </Button>
+        {user?.profileCompleted && (
+          <Menu userImage={user.image} userName={user.userName} />
         )}
       </div>
     </header>
