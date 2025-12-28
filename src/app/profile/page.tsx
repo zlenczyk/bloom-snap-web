7969bf56c91528f4394b6e39d2be3daec2927e6f
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-12-28 18:31:04 +0100

    create edit profile page.tsx

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,33 @@
+import { auth } from "@/auth";
+import db from "@/lib/db/db";
+import { redirect } from "next/navigation";
+import EditProfileForm from "./EditProfileForm";
+
+export default async function ProfilePage() {
+  const session = await auth();
+
+  if (!session?.user?.id) redirect("/sign-in");
+
+  const user = await db.user.findUnique({
+    where: { id: session.user.id },
+    select: {
+      userName: true,
+      image: true,
+      createdAt: true,
+      plants: { select: { id: true } },
+    },
+  });
+
+  if (!user) redirect("/sign-in");
+
+  return (
+    <div className="max-w-7xl mx-auto w-full layout-padding">
+      <EditProfileForm
+        userName={user.userName}
+        image={user.image}
+        createdAt={user.createdAt.toISOString()}
+        plantsCount={user.plants.length}
+      />
+    </div>
+  );
+}
