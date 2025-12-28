ed34031aceb5c025faaed5542fbef41a21674ee0
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-12-28 19:13:03 +0100

    create TimelineIcon component

---
+++ src/app/my-collection/[id]/Timeline/TimelineIcon.tsx
@@ -0,0 +1,16 @@
+import { TimelineEvent } from "@/lib/db/schema";
+import { IconRenderer } from "./IconRenderer";
+
+interface TimelineIconProps {
+  event: TimelineEvent;
+}
+
+export function TimelineIcon({ event }: TimelineIconProps) {
+  return (
+    <div
+      className={`absolute left-6 sm:left-1/2 transform -translate-x-1/2 w-10 h-10 sm:w-12 sm:h-12 rounded-full ${event.color} flex items-center justify-center shadow-lg z-10`}
+    >
+      <IconRenderer icon={event.icon} className="w-5 h-5 text-white" />
+    </div>
+  );
+}
