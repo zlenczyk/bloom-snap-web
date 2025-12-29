f8c1d8433b669c7cf11c5147bb326938f55f8c32
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-12-29 01:48:07 +0100

    adjust collection details page

--- src/app/my-collection/[id]/page.tsx
+++ src/app/my-collection/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { getTimelineEvents } from "@/lib/db/queries/getTimelineEvents";
-import { getPlantDetails } from "./PlantDetails/actions";
+import { getTimelineEvents } from "@/app/my-collection/[id]/actions/getTimelineEvents";
+import { getPlantDetails } from "./actions/getPlantDetails";
 import PlantDetails from "./PlantDetails/PlantDetails";
 import Timeline from "./Timeline/Timeline";
 
@@ -8,16 +8,21 @@ interface PageProps {
 }
 
 const Page = async ({ params }: PageProps) => {
-  const events = await getTimelineEvents();
-
   const { id: plantId } = await params;
+
   const plant = await getPlantDetails(plantId);
+  const events = await getTimelineEvents(plantId);
 
   return (
-    <div className="w-full">
-      <PlantDetails plant={plant} />
-      <Timeline initialEvents={events} />
-    </div>
+    <>
+      <div className="bg-zinc-100 sm:px-6 py-6 sm:py-8">
+        <PlantDetails plant={plant} />
+      </div>
+
+      <div className="sm:px-6 py-6 sm:py-8">
+        <Timeline events={events} plantId={plantId} />
+      </div>
+    </>
   );
 };
 
