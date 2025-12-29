eeb71871566b61ad499b23726392f2e6f68227bc
Zuzanna Lenczyk - Wąsowska <zuzia.lenczyk@gmail.com>
2025-12-29 01:47:09 +0100

    adjust menu

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -15,8 +15,8 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 type Props = {
-  userImage: string;
-  userName: string;
+  userImage: string | null;
+  userName: string | null;
 };
 
 const Menu = ({ userImage, userName }: Props) => {
@@ -29,35 +29,47 @@ const Menu = ({ userImage, userName }: Props) => {
   };
 
   return (
-    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+    <DropdownMenu open={isOpen} modal={false} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="p-0 h-auto hover:bg-inherit">
           <div className="flex items-center space-x-3">
-            <div className="relative w-10 h-10 rounded-full overflow-hidden">
-              <Image
-                src={userImage}
-                alt={userName}
-                fill
-                className="object-cover"
-              />
+            <div className="relative w-10 h-10 sm:w-12 sm:h-12 rounded-full overflow-hidden bg-transparent">
+              {userImage ? (
+                <Image
+                  src={userImage}
+                  alt="avatar"
+                  fill
+                  className="object-cover"
+                  priority
+                />
+              ) : (
+                <div className="w-full h-full flex items-center justify-center bg-green-500">
+                  <span className="text-white font-semibold text-lg sm:text-xl">
+                    {userName?.charAt(0).toUpperCase()}
+                  </span>
+                </div>
+              )}
             </div>
-            <span className="text-sm font-medium">{userName}</span>
+
+            <span className="hidden sm:inline-block text-sm font-medium">
+              {userName}
+            </span>
             <ChevronDown
-              className={`h-4 w-4 transition-transform ${
+              className={`h-5 w-5 transition-transform ${
                 isOpen ? "rotate-180" : ""
               }`}
             />
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
+      <DropdownMenuContent align="end" className="w-44 z-100">
         <DropdownMenuItem onClick={editProfile}>
-          <Settings className="mr-2 h-4 w-4" />
+          <Settings className="h-4 w-4" />
           <span>Edit Profile</span>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={logOut}>
-          <LogOut className="mr-2 h-4 w-4" />
+          <LogOut className="h-4 w-4" />
           <span>Sign Out</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
